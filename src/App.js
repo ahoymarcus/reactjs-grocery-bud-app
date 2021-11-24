014a964733dc1d9bb0ebe7eafc0a5cc43e9651c9
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-24 17:41:20 -0300

    Iniciar repositorio local

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// https://www.youtube.com/watch?v=ly3m6mv5qvg
+// 3 hr 51' 00 ''
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
+
+function App() {
+  return <h2>grocery bud setup</h2>
+}
+
+export default App
