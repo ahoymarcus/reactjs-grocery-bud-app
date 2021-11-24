f68342659cab28199a56f6a819d8cf65aba4b709
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-24 18:13:13 -0300

    Criar a estrutura basica de Jsx de App.js. Criar duas variaveis de estado e com elas definir handlers para Input e para Submit.

--- src/Alert.js
+++ src/Alert.js
@@ -1,7 +1,15 @@
 import React, { useEffect } from 'react'
 
+
+
 const Alert = () => {
+
+
   return <h2>alert component</h2>
 }
 
+
+
 export default Alert
+
+
--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 // https://www.youtube.com/watch?v=ly3m6mv5qvg
 // 3 hr 51' 00 ''
 import React, { useState, useEffect } from 'react'
+
 import List from './List'
 import Alert from './Alert'
 
+
+
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [ item, setItem ] = useState('');
+  const [ list, setList ] = useState([]);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(item);
+
+    setList(item);
+  };
+
+
+  return (
+    <main>
+      <h3>Grocery Bud App</h3>
+      <form onSubmit={handleSubmit} >
+        <input 
+          type="text"
+          placeholder="e.g. eggs"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <button >Submit</button>
+      </form>
+      <button>Clear items</button>
+    </main>
+  );
 }
 
+
+
 export default App
+
+
